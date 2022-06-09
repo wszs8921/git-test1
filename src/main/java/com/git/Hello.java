d5c37a9115c510dd105011a893101325ffa4346d
Immo <adw0524@163.com>
2022-06-09 22:40:21 +0800

    push test2 commit

--- src/main/java/com/git/Hello.java
+++ src/main/java/com/git/Hello.java
@@ -12,7 +12,7 @@ public class Hello
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
-        System.out.println("test push");
+        System.out.println("test push2");
         System.out.println("test pull2");
 
     }
