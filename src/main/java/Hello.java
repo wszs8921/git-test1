ad487b656efd7283da074c87b8e25a7c5d2759bb
Immo <adw0524@163.com>
2022-06-09 22:48:10 +0800

    first commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @author adwstart
+ * @created 2022-06-09 22:45
+ */
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("new branch test");
+    }
+}
